baecab0899383987b44e2cac32393ad7cf8df65a
davidmaoooooo <96400648+davidmaoooooo@users.noreply.github.com>
2026-06-20 08:38:40 +0800

    修复GitHub Actions构建：优先使用官方Maven源

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,29 +1,28 @@
 pluginManagement {
     repositories {
+        gradlePluginPortal()
+        google()
+        mavenCentral()
+        maven("https://jitpack.io")
         maven("https://maven.aliyun.com/repository/gradle-plugin/")
         maven("https://maven.aliyun.com/repository/google/")
         maven("https://maven.aliyun.com/repository/central/")
         maven("https://maven.aliyun.com/repository/public/")
         maven("https://mirrors.cloud.tencent.com/nexus/repository/maven-public/")
-        gradlePluginPortal()
-        google()
-        mavenCentral()
-        maven("https://jitpack.io")
     }
 }
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
-        maven("https://maven.aliyun.com/repository/google/")
-        maven("https://maven.aliyun.com/repository/central/")
-        maven("https://maven.aliyun.com/repository/public/")
-        maven("https://mirrors.cloud.tencent.com/nexus/repository/maven-public/")
         google()
         mavenCentral()
         maven("https://jitpack.io")
         maven("https://repo1.maven.org/maven2/")
-        // banner
         maven("https://s01.oss.sonatype.org/content/groups/public")
+        maven("https://maven.aliyun.com/repository/google/")
+        maven("https://maven.aliyun.com/repository/central/")
+        maven("https://maven.aliyun.com/repository/public/")
+        maven("https://mirrors.cloud.tencent.com/nexus/repository/maven-public/")
     }
 }
 rootProject.name = "音乐 Music"
