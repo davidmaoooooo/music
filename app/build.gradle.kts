cf1d3f3751c9266ad38d78c645ef43b1a1cfb8c1
davidmaoooooo <96400648+davidmaoooooo@users.noreply.github.com>
2026-06-20 08:34:25 +0800

    改包名为 com.apple.android.music,适配originos

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,7 +20,7 @@ android {
     compileSdk = libs.versions.compileSdk.get().toInt()
 
     defaultConfig {
-        applicationId = "com.david.music"
+        applicationId = "com.apple.android.music"
         minSdk = libs.versions.minSdk.get().toInt()
         targetSdk = libs.versions.targetSdk.get().toInt()
         versionCode = libs.versions.versionCode.get().toInt()
